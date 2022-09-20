2a19683a03aaa5148918b8df4a927496d26e8d4e
wormsJJG <jjr66461837@gmail.com>
2022-09-20 22:10:13 +0900

    feat: Firebase 셋팅

--- letToDo/letToDo/Delegate/AppDelegate.swift
+++ letToDo/letToDo/Delegate/AppDelegate.swift
@@ -6,13 +6,16 @@
 //
 
 import UIKit
+import FirebaseCore
 
-@main
+
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
