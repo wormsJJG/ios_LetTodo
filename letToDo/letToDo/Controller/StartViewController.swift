17d3622f0877f039626a75e8796af30b6e885ac2
wormsJJG <jjr66461837@gmail.com>
2022-09-21 14:36:08 +0900

    feat: 아이디, 비밀번호 textfield배치

--- letToDo/letToDo/Controller/StartViewController.swift
+++ letToDo/letToDo/Controller/StartViewController.swift
@@ -11,63 +11,106 @@ import SnapKit
 class StartViewController: UIViewController {
     private let viewModel = StartViewModel()
     
-    let appNameLabel: UILabel = {
+    let hiLabel: UILabel = {
         let label = UILabel()
-        label.text = "Let-To-DO"
+        label.text = "안녕하세요 :)"
         label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 40)
-        label.textColor = .blue
+        label.font = UIFont.systemFont(ofSize: 34)
+        label.textColor = .black
         return label
+    }()
+    let IntroduceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "LetToDO입니다."
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 34)
+        label.textColor = .black
+        return label
+    }()
+    let idTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "아이디를 입력해주세요."
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "비밀번호를 입력해주세요."
+        textField.borderStyle = .roundedRect
+        
+        return textField
     }()
     let goLoginButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("로그인 하기", for: .normal)
-        button.layer.cornerRadius = 21
+        button.layer.cornerRadius = 10
         button.setTitleColor(.black, for: .normal)
-        button.backgroundColor = .systemGray5
+        button.backgroundColor = .white
         button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
-        button.layer.borderWidth = 3
+        button.layer.borderWidth = 1
         return button
     }()
     let goRegisterButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("아직 회원이 아니십니까?", for: .normal)
-        button.layer.cornerRadius = 21
+        button.layer.cornerRadius = 10
         button.setTitleColor(.black, for: .normal)
-        button.backgroundColor = .systemGray5
+        button.backgroundColor = .white
         button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
-        button.layer.borderWidth = 3
+        button.layer.borderWidth = 1
         return button
     }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         viewControllerSetting()
     }
     
     fileprivate func viewControllerSetting() {
-        view.addSubview(appNameLabel)
+        view.addSubview(hiLabel)
         view.addSubview(goLoginButton)
         view.addSubview(goRegisterButton)
+        view.addSubview(IntroduceLabel)
+        view.addSubview(idTextField)
+        view.addSubview(passwordTextField)
         layout()
     }
     
     fileprivate func layout() {
-        appNameLabel.snp.makeConstraints { (label) in
+        hiLabel.snp.makeConstraints { (label) in
             label.width.equalTo(200)
             label.height.equalTo(50)
-            label.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(100)
-            label.centerX.equalToSuperview()
+            label.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(80)
+            label.left.equalTo(self.view.snp.left).offset(10)
+        }
+        IntroduceLabel.snp.makeConstraints { (label) in
+            label.width.equalTo(250)
+            label.height.equalTo(50)
+            label.top.equalTo(hiLabel.snp.bottom).offset(20)
+            label.left.equalTo(self.view.snp.left).offset(10)
+        }
+        idTextField.snp.makeConstraints { (textField) in
+            textField.width.equalTo(300)
+            textField.height.equalTo(50)
+            textField.top.equalTo(IntroduceLabel.snp.bottom).offset(80)
+            textField.centerX.equalToSuperview()
+        }
+        passwordTextField.snp.makeConstraints { (textField) in
+            textField.width.equalTo(300)
+            textField.height.equalTo(50)
+            textField.top.equalTo(idTextField.snp.bottom).offset(10)
+            textField.centerX.equalToSuperview()
         }
         goLoginButton.snp.makeConstraints { (button) in
             button.width.equalTo(300)
             button.height.equalTo(50)
-            button.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).offset(-180)
+            button.bottom.equalTo(passwordTextField.snp.bottom).offset(70)
             button.centerX.equalToSuperview()
         }
         goRegisterButton.snp.makeConstraints { (button) in
             button.width.equalTo(goLoginButton.snp.width)
             button.height.equalTo(goLoginButton.snp.height)
-            button.top.equalTo(goLoginButton.snp.bottom).offset(20)
+            button.top.equalTo(goLoginButton.snp.bottom).offset(10)
             button.centerX.equalToSuperview()
         }
     }
@@ -92,7 +135,7 @@ struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
             ViewControllerRepresentable()
                 .ignoresSafeArea()
                 .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
-                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+                .previewDevice(PreviewDevice(rawValue: "iPhone 13 pro"))
         }
         
     }
